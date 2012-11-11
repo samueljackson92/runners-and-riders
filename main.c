f354b1435ed97daa33db8fa8d1d13832c710e08d
Samuel Jackson <slj11@aber.ac.uk>
2012-11-11 14:34:43 +0000

    Initial commit of netbeans project files.

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Samuel Jackson
+ *
+ * Created on 11 November 2012, 14:31
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
