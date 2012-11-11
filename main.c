09817e4da1969f2180bc8e2f8bf1d0d65a0b053a
Samuel Jackson <slj11@aber.ac.uk>
2012-11-11 16:21:04 +0000

    Added functionality to read text files. Read of files currently not working. Compiled C program cannot find the specified file. Possibly a netbeans config issue.

--- main.c
+++ main.c
@@ -8,11 +8,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "main.h"
+
 /*
  * 
  */
 int main(int argc, char** argv) {
 
+    read_file_names();
     return (EXIT_SUCCESS);
+}
+
+void read_file_names(){
+    char event_details_file[50];
+    char event_nodes_file[MAX_FILENAME_LENGTH];
+    char event_tracks_file[MAX_FILENAME_LENGTH];
+    char event_course_file[MAX_FILENAME_LENGTH];
+    char event_entrants_file[MAX_FILENAME_LENGTH];
+    
+    printf("Enter name of event details file:");
+    scanf(" %s", event_details_file);
+    
+    /*
+    scanf("\nEnter name of event nodes file:\n", event_nodes_file);
+    scanf("\nEnter name of event tracks file:\n", event_tracks_file);
+    scanf("\nEnter name of event courses file:\n", event_course_file);
+    scanf("\nEnter name of event entrants file:\n", event_entrants_file);*/
+    
+    read_file(event_details_file, &read_event_details);
+    
+}
+
+void read_file(char name[MAX_FILENAME_LENGTH], void (*read_file_func) (FILE *)){
+   
+    FILE *file;
+    file = fopen(name, "r");
+    if(file != NULL) {
+        /*file opened successfully, read it.*/
+        read_file_func(file);
+    } else {
+        puts("Error opening file!");
+    }
+    fclose(file);
+}
+
+void read_event_details(FILE *file){
+    Event e;
+
+    /*read in the details for the event*/
+    fgets(e.name, sizeof(e.name), file);
+    fgets(e.date, sizeof(e.date), file);
+    fgets(e.start_time, sizeof(e.start_time), file);
+    
+    printf("%s", e.name);
 }
 
---
+++ main.h
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.h
+ * Author: samuel
+ *
+ * Created on 11 November 2012, 15:01
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define MAX_FILENAME_LENGTH 50
+#define MAX_EVENT_NAME_SIZE 50
+#define MAX_DATE_SIZE 20
+#define TIME_STRING_SIZE 5
+
+/*read in the name of each of the data files from the user.*/
+void read_file_names();
+
+/*open a file for reading with the given name and a function to read the contents.*/
+void read_file(char name[MAX_FILENAME_LENGTH], void (*read_file_func) (FILE *));
+
+/*function to read event details from a file.*/
+void read_event_details(FILE *);
+
+/*structure to hold details about a single event*/
+typedef struct event {
+    char name[MAX_EVENT_NAME_SIZE];
+    char date[MAX_DATE_SIZE];
+    char start_time[TIME_STRING_SIZE];
+} Event;
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MAIN_H */
+
